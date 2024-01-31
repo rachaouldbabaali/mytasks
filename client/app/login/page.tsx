5d0d6db46548fbb9c7a5f62f1364a2d13f838864
Racha OULDBABAALI <rachaould@gmail.com>
2024-01-31 23:09:06 +0100

    Replace form by formik

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,7 +1,9 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { useRouter } from "next/navigation";
 import { login } from "../api/api";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -10,30 +12,36 @@ interface LoginProps {
 }
 
 const LoginPage: React.FC<LoginProps> = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const router = useRouter();
-  const onLogin = async (email: string, password: string) => {
-    const response = await login(email, password);
 
-    if (response.status === 200) {
-      toast.success("Login successful!");
-    router.push("/allTasks");
-    } else {
-        toast.error("Login failed!");
-        }
-
+  const initialValues = {
+    email: "",
+    password: "",
   };
 
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string().required("Required"),
+  });
 
-  const handleLogin = (e: SyntheticEvent) => {
-    e.preventDefault();
-    if (email && password) {
-      onLogin(email, password);
-    } else {
-      toast.error("Please enter valid email and password");
+  const onSubmit = async (values: typeof initialValues) => {
+    const { email, password } = values;
+
+    try {
+      const response = await login(email, password);
+
+      if (response.status === 200) {
+        toast.success("Login successful!");
+        router.push("/allTasks");
+      } else {
+        toast.error("Login failed!");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      toast.error("An error occurred during login.");
     }
   };
+
   const handleSignUp = () => {
     router.push("/register");
   };
@@ -44,59 +52,74 @@ const LoginPage: React.FC<LoginProps> = () => {
         <ToastContainer />
       </div>
       <div className="min-h-screen flex items-center justify-center">
-        <form
-          className="bg-white p-8 rounded shadow-md w-96"
-          onSubmit={handleLogin}
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
         >
-          <h2 className="text-2xl font-bold mb-4">Login</h2>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="w-full p-2 border rounded-md"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="w-full p-2 border rounded-md"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button
-            className="bg-blue-500 text-white p-2 rounded-md w-full hover:bg-blue-600"
-            type="submit"
-          >
-            Login
-          </button>
-          <div className="mt-4">
+          <Form className="bg-white p-8 rounded shadow-md w-96">
+            <h2 className="text-2xl font-bold mb-4">Login</h2>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                className="w-full p-2 border rounded-md"
+                placeholder="Enter your email"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="text-red-500 text-sm mt-1"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <Field
+                type="password"
+                id="password"
+                name="password"
+                className="w-full p-2 border rounded-md"
+                placeholder="Enter your password"
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="text-red-500 text-sm mt-1"
+              />
+            </div>
+
             <button
-              className="text-blue-500 hover:underline"
-              type="button"
-              onClick={handleSignUp}
+              className="bg-blue-500 text-white p-2 rounded-md w-full hover:bg-blue-600"
+              type="submit"
             >
-              Don't have an account? Sign Up
+              Login
             </button>
-          </div>
-        </form>
+
+            <div className="mt-4">
+              <button
+                className="text-blue-500 hover:underline"
+                type="button"
+                onClick={handleSignUp}
+              >
+                Don't have an account? Sign Up
+              </button>
+            </div>
+          </Form>
+        </Formik>
       </div>
     </>
   );
--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -1,6 +1,8 @@
 "use client"
-import React, { useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/navigation';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import { register } from '../api/api';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -10,109 +12,119 @@ interface SignUpProps {
 }
 
 const SignUpPage: React.FC<SignUpProps> = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
   const router = useRouter();
-  
-  const handleSignUp = async (e: SyntheticEvent) => {
-      e.preventDefault();
-    if (name && email && password) {
-      onSignUp(name, email, password);
-    } else {
-        toast.error('Please enter valid name, email, and password');
-    }};
 
-  const onSignUp = async (name: string, email: string, password: string) => {
+  const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const onSubmit = async (values: typeof initialValues) => {
+    const { name, email, password } = values;
+
     try {
-        const response = await register(name, email, password);
-        
-        if (response.status === 201) {
-          toast.success('Signup successful!');
-          router.push('/login');
-        } else if (response.status === 409) {
-          toast.error('Email already exists!');
-        } else {
-          toast.error('Signup failed!');
-        }
-      } catch (error) {
-        console.error('Error during signup:', error);
-        if (error.response) {
-          toast.error(error.response.data.message);
-        } else {
-          toast.error('Something went wrong!');
-        }
+      const response = await register(name, email, password);
+
+      if (response.status === 201) {
+        toast.success('Signup successful!');
+        router.push('/login');
+      } else if (response.status === 409) {
+        toast.error('Email already exists!');
+      } else {
+        toast.error('Signup failed!');
       }
+    } catch (error) {
+      console.error('Error during signup:', error);
+      if (error.response) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error('Something went wrong!');
+      }
+    }
   };
 
   return (
     <>
-    <div>
-      <ToastContainer />
-    </div>
-    <div className="min-h-screen flex items-center justify-center">
-      <form 
-        className="bg-white p-8 rounded shadow-md w-96"
-        onSubmit={handleSignUp}
-      >
-        <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="w-full p-2 border rounded-md"
-            placeholder="Enter your name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="w-full p-2 border rounded-md"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="w-full p-2 border rounded-md"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button
-          className="bg-blue-500 text-white p-2 rounded-md w-full hover:bg-blue-600"
-          type="submit"
+      <div>
+        <ToastContainer />
+      </div>
+      <div className="min-h-screen flex items-center justify-center">
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
         >
-          Sign Up
-        </button>
-        <div className="mt-4">
-          <button
-            className="text-blue-500 hover:underline"
-            type="button"
-            onClick={() => router.push('/login')}
-          >
-            Already have an account? Login
-          </button>
-        </div>
-      </form>
-    </div>
+          <Form className="bg-white p-8 rounded shadow-md w-96">
+            <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                Name
+              </label>
+              <Field
+                type="text"
+                id="name"
+                name="name"
+                className="w-full p-2 border rounded-md"
+                placeholder="Enter your name"
+              />
+              <ErrorMessage name="name" component="div" className="text-red-500 text-sm mt-1" />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                Email
+              </label>
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                className="w-full p-2 border rounded-md"
+                placeholder="Enter your email"
+              />
+              <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                Password
+              </label>
+              <Field
+                type="password"
+                id="password"
+                name="password"
+                className="w-full p-2 border rounded-md"
+                placeholder="Enter your password"
+              />
+              <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
+            </div>
+
+            <button
+              className="bg-blue-500 text-white p-2 rounded-md w-full hover:bg-blue-600"
+              type="submit"
+            >
+              Sign Up
+            </button>
+
+            <div className="mt-4">
+              <button
+                className="text-blue-500 hover:underline"
+                type="button"
+                onClick={() => router.push('/login')}
+              >
+                Already have an account? Login
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
     </>
   );
 };
