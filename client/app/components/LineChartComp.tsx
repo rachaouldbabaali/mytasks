cc9c7d6009b179be9b6121a04d97a4594310594e
Racha OULDBABAALI <rachaould@gmail.com>
2024-02-01 02:55:44 +0100

    Add responsive width

--- client/app/components/LineChartComp.tsx
+++ client/app/components/LineChartComp.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getNumberOfTasksByMonth } from '../api/api';
-import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
 
 const LineChartComp = () => {
   const [monthlyData, setMonthlyData] = useState([]);
@@ -17,7 +17,8 @@ const LineChartComp = () => {
       <h2 className="text-xl font-bold p-4 text-gray-700 mt-4 text-start">
         Monthly Completion Rate</h2>
       {monthlyData && Object.keys(monthlyData).length > 0 ? (
-        <LineChart width={600} height={300} data={Object.entries(monthlyData)}>
+        <ResponsiveContainer width="100%" height={300}>
+        <LineChart data={Object.entries(monthlyData)}>
           <XAxis dataKey="[0]" />
           <YAxis />
           <CartesianGrid strokeDasharray="3 3" />
@@ -26,6 +27,7 @@ const LineChartComp = () => {
           <Line type="monotone" dataKey="[1].completed" name="Completed" stroke="#0088FE" />
           <Line type="monotone" dataKey="[1].incompleted" name="Incompleted" stroke="#FF8042" />
         </LineChart>
+        </ResponsiveContainer>
       ) : (
         <p>No data available</p>
       )}
